495b9dc4a4dbf98ee4beb69758c80dc7086c3549
James Mobus <mobusj@live.com>
2020-05-29 17:09:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(time) {
-  hour = time.split(":")
+  let hour = time.split(":")[0]
   
   if (hour < 12) {
     return "Good Morning"
